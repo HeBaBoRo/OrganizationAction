5328463fa7a28c3f46c45b20a3eb137155a70570
bobby.rollof <hbbrollof@gmail.com>
2023-05-20 23:03:47 +0200

    Trying something 8

--- generateFiles/generateFiles.py
+++ generateFiles/generateFiles.py
@@ -3,6 +3,7 @@ import os
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 def renderTemplate(dataFile, templateFolder, outputFolder="generated"):
+    data = "Some string"
     with open("/".join([outputFolder, dataFile]), "r") as file:
         try:
             data = yaml.safe_load(file)
