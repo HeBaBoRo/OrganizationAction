33dbbe9d9aa12f4f3d702d4449559770d6c45aba
bobby.rollof <hbbrollof@gmail.com>
2023-05-20 22:53:32 +0200

    Trying something 5

--- generateFiles/joinConfig.sh
+++ generateFiles/joinConfig.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-rm -f ${{ env.GENERATED_CODE_LOCATION }}/${{ env.CONFIG_VARIABLES_TARGET_FILE_NAME }}
-touch ${{ env.GENERATED_CODE_LOCATION }}/${{ env.CONFIG_VARIABLES_TARGET_FILE_NAME }}
-cat ${{ env.CONFIG_VARIABLES_FOLDER }}/members.yml ${{ env.CONFIG_VARIABLES_FOLDER }}/teams.yml ${{ env.CONFIG_VARIABLES_FOLDER }}/github.yml ${{ env.CONFIG_VARIABLES_FOLDER }}/datadog.yml ${{ env.CONFIG_VARIABLES_FOLDER }}/discord.yml ${{ env.CONFIG_VARIABLES_FOLDER }}/terraformCloud.yml > ${{ env.GENERATED_CODE_LOCATION }}/${{ env.CONFIG_VARIABLES_TARGET_FILE_NAME }}
+LOCATION_CONFIG_FILES=$1;
+OUTPUT_LOCATION=$2;
+CONFIG_FILENAME=$3;
+
+rm -f $OUTPUT_LOCATION/$CONFIG_FILENAME
+touch $OUTPUT_LOCATION/$CONFIG_FILENAME
+
+for configFile in $LOCATION_CONFIG_FILES/*.yml;
+do
+  echo "Processing $f file...";
+  cat $configFile >> $OUTPUT_LOCATION/$CONFIG_FILENAME
+done
